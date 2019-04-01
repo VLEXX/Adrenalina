89140691bf4445481abf18ebc992a4af618ed6b8
Federico Scatà <federico.scata@mail.polimi.it>
2019-04-01 14:23:59 +0200

    Aggiunto classe e test "VoteMap"

---
+++ src/main/java/Model/VoteMap.java
@@ -0,0 +1,27 @@
+package Model;
+
+public class VoteMap {
+    private int[] VoteResult;
+    private int FinalResult;
+
+    public VoteMap(){
+        VoteResult = new int[]{0,0,0,0};
+        FinalResult=-1;
+    }
+
+    public int getFinalResult() {
+        return FinalResult;
+    }
+
+    public void setFinalResult(int finalResult) {
+        FinalResult = finalResult;
+    }
+
+    public int[] getVoteResult() {
+        return VoteResult;
+    }
+
+    public void setVoteResult(int index) {
+        VoteResult[index] = VoteResult[index]+1;
+    }
+}

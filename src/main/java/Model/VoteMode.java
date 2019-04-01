1f4d1ce7836e1b82176deddd0c8b3ab619e3b2b1
Federico Scatà <federico.scata@mail.polimi.it>
2019-04-01 16:40:25 +0200

    Aggiunto classe e relativo test "VoteMode"

---
+++ src/main/java/Model/VoteMode.java
@@ -0,0 +1,27 @@
+package Model;
+
+public class VoteMode {
+    private int[] VoteResult;
+    private int FinalResult;
+
+    public VoteMode(){
+        VoteResult = new int[]{0,0,0,0};
+        FinalResult=-1;
+    }
+
+    public int getFinalResult() {
+        return FinalResult;
+    }
+
+    public void setFinalResult(int finalResult) {
+        FinalResult = finalResult;
+    }
+
+    public int[] getVoteResult() {
+        return VoteResult;
+    }
+
+    public void setVoteResult(int index) {
+        VoteResult[index] = VoteResult[index]+1;
+    }
+}

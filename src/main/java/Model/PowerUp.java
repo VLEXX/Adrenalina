5c04af2a71a07001bc98923e8c095c7c70b45688
Federico Scatà <federico.scata@mail.polimi.it>
2019-04-02 11:24:51 +0200

    Aggiunto classe "PowerUp" e relativo test

---
+++ src/main/java/Model/PowerUp.java
@@ -0,0 +1,13 @@
+package Model;
+
+public class PowerUp {
+    private String PowerUpName;
+
+    public PowerUp(String name){
+        this.PowerUpName=name;
+    }
+
+    public String getPowerUpName() {
+        return PowerUpName;
+    }
+}
---
+++ src/test/java/Model/PowerUpTest.java
@@ -0,0 +1,14 @@
+package Model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PowerUpTest {
+
+    @Test
+    void getPowerUpName() {
+        PowerUp p = new PowerUp("name");
+        assertEquals(p.getPowerUpName(), "name");
+    }
+}

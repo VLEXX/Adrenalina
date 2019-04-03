2e3d017b2bc30fabf06bb2ba4041b580323f40df
Federico Scatà <federico.scata@mail.polimi.it>
2019-04-03 12:12:57 +0200

    Aggiunto enum "PowerUp" e "Weapons". Coverage delle enum 0% per ora, in attesa di classe "PlayerBoard".

---
+++ src/main/java/Model/PowerUp.java
@@ -0,0 +1,12 @@
+//Author: Federico Scatà
+package Model;
+
+//Enum che identifica i vari potenziamenti
+public enum PowerUp {
+    TargetingScope, //Mirino
+    Newton,         //Raggio Cinetico
+    TagbackGrenade, //Granata Venom
+    Teleporter      //Teletraporto
+}
+
+//Coverage nullo per ora. In attesa di classe PlayerBoard (CANCELLARE QUESTO COMMENTO QUANDO OK)

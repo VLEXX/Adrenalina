c0ab00a320e15726e52ec332510c5054be339a99
Alex Saletti <alex.saletti@mail.polimi.it>
2019-05-04 13:01:10 +0200

    Modificate le classi InizializeMap1/2/3/4

--- src/main/java/Model/InitializeMap1.java
+++ src/main/java/Model/InitializeMap1.java
@@ -57,6 +57,7 @@ public class InitializeMap1 implements Strategy {
         cell11.setCells(cell8,null,cell10,null);
         //settaggio celle raggiungibili
         map1.getRoomList().forEach(room -> room.getCellsList().forEach(cell -> cell.initializeReachableCells()));
+
         return map1;
     }
 }

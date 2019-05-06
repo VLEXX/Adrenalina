9082c49977db5a4127d5e3514caea75ab13884ad
Federico Scatà <federico.scata@mail.polimi.it>
2019-05-06 18:14:59 +0200

    Commentato MoveState

--- src/main/java/Model/MoveState.java
+++ src/main/java/Model/MoveState.java
@@ -10,7 +10,7 @@ public class MoveState implements State {
     public void doAction(PrintWriter printWriter, Scanner scanner, InitializeAllPlay i, Player p) {
         printWriter.println("Choose a cell...");
         String s = new String();
-        while(true){
+        while(true){                    //Server attende ID della cella in cui spostarsi dal client.
             if(s.isEmpty()) {
                 s = scanner.nextLine();
             }
@@ -18,7 +18,8 @@ public class MoveState implements State {
                 break;
             }
         }
-        Cell c;
+        //Qui ci va un metodo per prelevare la cella corrispondente all'ID ricevuto dal client.
+        Cell c; //Qui bisogna inserire la cella prelevata Cell c = metodo(id);
         i.getCurrentPlayerState().forEach(ps -> {
             if(ps.isActiveturn() && ps.getActiveplayer()==p){
                 if (ps.getPlayerposition().getCurrentcell().getReachableCells().contains(c)){

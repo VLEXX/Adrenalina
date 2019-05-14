e47daf31609093a32530180ea137c50ea038de7c
Federico Scatà <federico.scata@mail.polimi.it>
2019-05-14 15:20:44 +0200

    Newton

---
+++ src/main/java/model/powerups/Newton.java
@@ -0,0 +1,11 @@
+package model.powerups;
+
+import model.munitions.Munitions;
+
+public class Newton extends PowerUp {
+
+    public Newton(Munitions m) {
+        super(PowerUpId.NEWTON, m);
+
+    }
+}

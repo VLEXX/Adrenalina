ffbf425ca045eb975a4c82087d6c99d156f794c8
Giulia Rivara <giulia.rivara@mail.polimi.it>
2019-05-27 22:39:57 +0200

    Shockwave completata

--- src/main/java/model/weaponscard/ShockWave.java
+++ src/main/java/model/weaponscard/ShockWave.java
@@ -4,6 +4,7 @@
 package model.weaponscard;
 
 import model.gamedata.InitializeAllPlay;
+import model.map.Cell;
 import model.map.Position;
 import model.munitions.Munitions;
 import model.playerdata.Player;
@@ -33,15 +34,124 @@ public class ShockWave extends Weapon implements Serializable {
         super.setName("shockwave");
     }
 
+    /**
+     * Function first attack
+     * @param myPlayer player who attack
+     * @param playerToAttack player to attack
+     * @param positionToMove null
+     * @param allPlay current state game
+     * @return OK or POSITION_NOT_VALID
+     */
     public MessageEnum firstAttack(Player myPlayer, ArrayList<Player> playerToAttack, Position positionToMove, InitializeAllPlay allPlay){
+        int control = 0;
+        Position positionToAttack1 = allPlay.getCurrentPlayerState().get(playerToAttack.get(0)).getPlayerposition();
+        Position myPosition = allPlay.getCurrentPlayerState().get(myPlayer).getPlayerposition();
+        Position positionToAttack2 = allPlay.getCurrentPlayerState().get(playerToAttack.get(1)).getPlayerposition();
+        Position positionToAttack3 = allPlay.getCurrentPlayerState().get(playerToAttack.get(2)).getPlayerposition();
+        if(playerToAttack.get(0) != null) {
+            if (checkPosition(myPosition.getCurrentcell(), positionToAttack1.getCurrentcell()) == false)
+                return MessageEnum.POSITION_NOT_VALID;
+        }
+        if(playerToAttack.get(1) != null) {
+            if (checkPosition(myPosition.getCurrentcell(), positionToAttack2.getCurrentcell()) == false)
+                return MessageEnum.POSITION_NOT_VALID;
+        }
+        if(playerToAttack.get(2) != null) {
+            if (checkPosition(myPosition.getCurrentcell(), positionToAttack3.getCurrentcell()) == false)
+                return MessageEnum.POSITION_NOT_VALID;
+        }
+        control = allPlay.getCurrentPlayerState().get(playerToAttack.get(0)).getBoard().getMarksBox().getMyMarksMap().get(myPlayer);
+        if(control != 0)
+            allPlay.getCurrentPlayerState().get(playerToAttack.get(0)).getBoard().getDamageBox().increaseDamage(control, myPlayer);
+        allPlay.getCurrentPlayerState().get(playerToAttack.get(0)).getBoard().getDamageBox().increaseDamage(1, myPlayer);
+        control = allPlay.getCurrentPlayerState().get(playerToAttack.get(1)).getBoard().getMarksBox().getMyMarksMap().get(myPlayer);
+        if(control != 0)
+            allPlay.getCurrentPlayerState().get(playerToAttack.get(1)).getBoard().getDamageBox().increaseDamage(control, myPlayer);
+        allPlay.getCurrentPlayerState().get(playerToAttack.get(1)).getBoard().getDamageBox().increaseDamage(1, myPlayer);
+        control = allPlay.getCurrentPlayerState().get(playerToAttack.get(2)).getBoard().getMarksBox().getMyMarksMap().get(myPlayer);
+        if(control != 0)
+            allPlay.getCurrentPlayerState().get(playerToAttack.get(2)).getBoard().getDamageBox().increaseDamage(control, myPlayer);
+        allPlay.getCurrentPlayerState().get(playerToAttack.get(2)).getBoard().getDamageBox().increaseDamage(1, myPlayer);
         return MessageEnum.OK;
     }
 
+    /**
+     * Function tsunami mode attack
+     * @param myPlayer player who attack
+     * @param playerToAttack player to attack
+     * @param positionToMove null
+     * @param allPlay current state game
+     * @return OK
+     */
     public MessageEnum secondAttack(Player myPlayer, ArrayList<Player> playerToAttack, Position positionToMove, InitializeAllPlay allPlay){
+        int control = 0;
+        Position myPosition = allPlay.getCurrentPlayerState().get(myPlayer).getPlayerposition();
+        if(myPosition.getCurrentcell().getUpCell() != null){
+            for(int i = 0; i < myPosition.getCurrentcell().getUpCell().getInCellPlayer().size(); i++){
+                control = allPlay.getCurrentPlayerState().get(myPosition.getCurrentcell().getUpCell().getInCellPlayer().get(i)).getBoard().getMarksBox().getMyMarksMap().get(myPlayer);
+                if(control != 0)
+                    allPlay.getCurrentPlayerState().get(myPosition.getCurrentcell().getUpCell().getInCellPlayer().get(i)).getBoard().getDamageBox().increaseDamage(control, myPlayer);
+                allPlay.getCurrentPlayerState().get(myPosition.getCurrentcell().getUpCell().getInCellPlayer().get(i)).getBoard().getDamageBox().increaseDamage(1, myPlayer);
+            }
+        }
+        if(myPosition.getCurrentcell().getDownCell() != null){
+            for(int i = 0; i < myPosition.getCurrentcell().getDownCell().getInCellPlayer().size(); i++){
+                control = allPlay.getCurrentPlayerState().get(myPosition.getCurrentcell().getUpCell().getInCellPlayer().get(i)).getBoard().getMarksBox().getMyMarksMap().get(myPlayer);
+                if(control != 0)
+                    allPlay.getCurrentPlayerState().get(myPosition.getCurrentcell().getUpCell().getInCellPlayer().get(i)).getBoard().getDamageBox().increaseDamage(control, myPlayer);
+                allPlay.getCurrentPlayerState().get(myPosition.getCurrentcell().getUpCell().getInCellPlayer().get(i)).getBoard().getDamageBox().increaseDamage(1, myPlayer);
+            }
+        }
+        if(myPosition.getCurrentcell().getRightCell() != null){
+            for(int i = 0; i < myPosition.getCurrentcell().getRightCell().getInCellPlayer().size(); i++){
+                control = allPlay.getCurrentPlayerState().get(myPosition.getCurrentcell().getUpCell().getInCellPlayer().get(i)).getBoard().getMarksBox().getMyMarksMap().get(myPlayer);
+                if(control != 0)
+                    allPlay.getCurrentPlayerState().get(myPosition.getCurrentcell().getUpCell().getInCellPlayer().get(i)).getBoard().getDamageBox().increaseDamage(control, myPlayer);
+                allPlay.getCurrentPlayerState().get(myPosition.getCurrentcell().getUpCell().getInCellPlayer().get(i)).getBoard().getDamageBox().increaseDamage(1, myPlayer);
+            }
+        }
+        if(myPosition.getCurrentcell().getLeftCell() != null){
+            for(int i = 0; i < myPosition.getCurrentcell().getLeftCell().getInCellPlayer().size(); i++){
+                control = allPlay.getCurrentPlayerState().get(myPosition.getCurrentcell().getUpCell().getInCellPlayer().get(i)).getBoard().getMarksBox().getMyMarksMap().get(myPlayer);
+                if(control != 0)
+                    allPlay.getCurrentPlayerState().get(myPosition.getCurrentcell().getUpCell().getInCellPlayer().get(i)).getBoard().getDamageBox().increaseDamage(control, myPlayer);
+                allPlay.getCurrentPlayerState().get(myPosition.getCurrentcell().getUpCell().getInCellPlayer().get(i)).getBoard().getDamageBox().increaseDamage(1, myPlayer);
+            }
+        }
         return MessageEnum.OK;
     }
 
     public MessageEnum thirdAttack(Player myPlayer, ArrayList<Player> playerToAttack, InitializeAllPlay allPlay){
         return MessageEnum.ATTACK_NOT_PRESENT;
+    }
+
+    /**
+     * Function check the correct position to shot
+     * @param current current cell of the player
+     * @param shot cell when the player want to go
+     * @return true if possible
+     */
+    private boolean checkPosition(Cell current, Cell shot) {
+        if (current.getUpCell() != null) {
+            if (current.getUpCell().getCellId() == shot.getCellId()) {
+                return true;
+            }
+        }
+        if (current.getDownCell() != null) {
+            if (current.getDownCell().getCellId() == shot.getCellId()) {
+                return true;
+            }
+        }
+        if (current.getLeftCell() != null) {
+            if (current.getLeftCell().getCellId() == shot.getCellId()) {
+                return true;
+            }
+        }
+        if (current.getRightCell() != null) {
+            if (current.getRightCell().getCellId() == shot.getCellId()) {
+                return true;
+            }
+        }
+        return false;
     }
 }

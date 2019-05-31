0537f66d70185fa11a1256e83909c966750798fd
Giulia Rivara <giulia.rivara@mail.polimi.it>
2019-05-31 09:47:43 +0200

    VortexCannon completata

--- src/main/java/model/weaponscard/VortexCannon.java
+++ src/main/java/model/weaponscard/VortexCannon.java
@@ -4,6 +4,7 @@
 package model.weaponscard;
 
 import model.gamedata.InitializeAllPlay;
+import model.map.Cell;
 import model.map.Position;
 import model.munitions.Munitions;
 import model.playerdata.Player;
@@ -17,6 +18,9 @@ import java.util.ArrayList;
  * weapon Vortexcannon
  */
 public class VortexCannon extends Weapon implements Serializable {
+
+    Position position;
+    Player player1;
 
     /**
      * Constructor that set the cost of this weapon
@@ -34,15 +38,130 @@ public class VortexCannon extends Weapon implements Serializable {
         super.setName("vortexcannon");
     }
 
-    public MessageEnum firstAttack(Player myPlayer, ArrayList<Player> playerToAttack, Position positionToMove, InitializeAllPlay allPlay){
+    /**
+     * Function first attack
+     * @param myPlayer player who attack
+     * @param playerToAttack player to attack
+     * @param positionVortex vortex
+     * @param allPlay current state game
+     * @return POSITION_UNREACHABLE or OK PLAYER_NOT_FOUND
+     */
+    public MessageEnum firstAttack(Player myPlayer, ArrayList<Player> playerToAttack, Position positionVortex, InitializeAllPlay allPlay){
+        int control = 0;
+        Position myPosition = allPlay.getCurrentPlayerState().get(myPlayer).getPlayerposition();
+        if(myPosition == positionVortex || check(myPosition,positionVortex) == false)
+            return MessageEnum.POSITION_UNREACHABLE;
+        Position positionToAttack = allPlay.getCurrentPlayerState().get(playerToAttack.get(0)).getPlayerposition();
+        if(checkPosition(positionVortex.getCurrentcell(),positionToAttack.getCurrentcell())){
+            control = allPlay.getCurrentPlayerState().get(playerToAttack.get(0)).getBoard().getMarksBox().getMyMarksMap().get(myPlayer);
+            if(positionVortex.getCurrentcell() != positionToAttack.getCurrentcell()) {
+                allPlay.getCurrentPlayerState().get(playerToAttack.get(0)).getPlayerposition().getCurrentcell().removeInCellPlayer(playerToAttack.get(0));
+                allPlay.getCurrentPlayerState().get(playerToAttack.get(0)).getPlayerposition().setCurrentcell(positionVortex.getCurrentcell());
+                allPlay.getCurrentPlayerState().get(playerToAttack.get(0)).getPlayerposition().getCurrentcell().addInCellPlayer(playerToAttack.get(0));
+            }
+            if(control != 0)
+                allPlay.getCurrentPlayerState().get(playerToAttack.get(0)).getBoard().getDamageBox().increaseDamage(control, myPlayer);
+            allPlay.getCurrentPlayerState().get(playerToAttack.get(0)).getBoard().getDamageBox().increaseDamage(2, myPlayer);
+        } else
+            return MessageEnum.PLAYER_NOT_FOUND;
+        position = positionVortex;
+        player1 = playerToAttack.get(0);
         return MessageEnum.OK;
     }
 
-    public MessageEnum secondAttack(Player myPlayer, ArrayList<Player> playerToAttack, Position positionToMove, InitializeAllPlay allPlay){
+    /**
+     * Function black hole
+     * @param myPlayer player who attack
+     * @param playerToAttack player to attack
+     * @param positionVortex vortex hole
+     * @param allPlay current state game
+     * @return PLAYERS_NOT_VALID or OK or PLAYER_NOT_FOUND
+     */
+    public MessageEnum secondAttack(Player myPlayer, ArrayList<Player> playerToAttack, Position positionVortex, InitializeAllPlay allPlay){
+        int control = 0;
+        if((playerToAttack.get(0) == playerToAttack.get(1)) || (playerToAttack.get(0) == player1) || (playerToAttack.get(1) == player1))
+            return MessageEnum.PLAYERS_NOT_VALID;
+        Position positionToAttack1 =  allPlay.getCurrentPlayerState().get(playerToAttack.get(0)).getPlayerposition();
+        Position positionToAttack2 = allPlay.getCurrentPlayerState().get(playerToAttack.get(1)).getPlayerposition();
+        if(checkPosition(position.getCurrentcell(), positionToAttack1.getCurrentcell()) && checkPosition(position.getCurrentcell(), positionToAttack2.getCurrentcell())){
+            //player 1
+            control = allPlay.getCurrentPlayerState().get(playerToAttack.get(0)).getBoard().getMarksBox().getMyMarksMap().get(myPlayer);
+            if(position.getCurrentcell() != positionToAttack1.getCurrentcell()) {
+                allPlay.getCurrentPlayerState().get(playerToAttack.get(0)).getPlayerposition().getCurrentcell().removeInCellPlayer(playerToAttack.get(0));
+                allPlay.getCurrentPlayerState().get(playerToAttack.get(0)).getPlayerposition().setCurrentcell(position.getCurrentcell());
+                allPlay.getCurrentPlayerState().get(playerToAttack.get(0)).getPlayerposition().getCurrentcell().addInCellPlayer(playerToAttack.get(0));
+            }
+            if(control != 0)
+                allPlay.getCurrentPlayerState().get(playerToAttack.get(0)).getBoard().getDamageBox().increaseDamage(control, myPlayer);
+            allPlay.getCurrentPlayerState().get(playerToAttack.get(0)).getBoard().getDamageBox().increaseDamage(1, myPlayer);
+            //player 2
+            control = allPlay.getCurrentPlayerState().get(playerToAttack.get(1)).getBoard().getMarksBox().getMyMarksMap().get(myPlayer);
+            if(position.getCurrentcell() != positionToAttack2.getCurrentcell()) {
+                allPlay.getCurrentPlayerState().get(playerToAttack.get(1)).getPlayerposition().getCurrentcell().removeInCellPlayer(playerToAttack.get(1));
+                allPlay.getCurrentPlayerState().get(playerToAttack.get(1)).getPlayerposition().setCurrentcell(position.getCurrentcell());
+                allPlay.getCurrentPlayerState().get(playerToAttack.get(1)).getPlayerposition().getCurrentcell().addInCellPlayer(playerToAttack.get(1));
+            }
+            if(control != 0)
+                allPlay.getCurrentPlayerState().get(playerToAttack.get(1)).getBoard().getDamageBox().increaseDamage(control, myPlayer);
+            allPlay.getCurrentPlayerState().get(playerToAttack.get(1)).getBoard().getDamageBox().increaseDamage(1, myPlayer);
+        }else
+            return MessageEnum.PLAYER_NOT_FOUND;
         return MessageEnum.OK;
     }
 
     public MessageEnum thirdAttack(Player myPlayer, ArrayList<Player> playerToAttack, InitializeAllPlay allPlay){
         return MessageEnum.ATTACK_NOT_PRESENT;
+    }
+
+    /**
+     * Function that check for correct position
+     * @param myPosition       position of the player who attack
+     * @param positionToAttack position of the player to attack
+     * @return true if ok
+     */
+    public boolean check(Position myPosition, Position positionToAttack) {
+        boolean find = false;
+        for (int i = 0; i < myPosition.getCurrentcell().getReachable3Cells().size(); i++) {
+            if (myPosition.getCurrentcell().getReachable3Cells().get(i).getCellId() == positionToAttack.getCurrentcell().getCellId()) {
+                find = true;
+                break;
+            }
+        }
+        if (find == false) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Function check the correct position to shot
+     * @param current current cell of the player
+     * @param shot cell when the player want to go
+     * @return true if possible
+     */
+    private boolean checkPosition(Cell current, Cell shot) {
+        if(current == shot)
+            return true;
+        if (current.getUpCell() != null) {
+            if (current.getUpCell().getCellId() == shot.getCellId()) {
+                return true;
+            }
+        }
+        if (current.getDownCell() != null) {
+            if (current.getDownCell().getCellId() == shot.getCellId()) {
+                return true;
+            }
+        }
+        if (current.getLeftCell() != null) {
+            if (current.getLeftCell().getCellId() == shot.getCellId()) {
+                return true;
+            }
+        }
+        if (current.getRightCell() != null) {
+            if (current.getRightCell().getCellId() == shot.getCellId()) {
+                return true;
+            }
+        }
+        return false;
     }
 }
